b337ee671b6d442665c3a47fe4daf52162b0d5eb
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-12-10 10:18:47 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        if (6 >= 3) {
+            System.out.println("Hello, World!");
+
+        }
+                }
+}
