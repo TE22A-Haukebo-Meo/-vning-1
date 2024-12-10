06d1cd11dbf7b79b105f416b5e50c290ab225ca7
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-12-10 10:21:20 +0100

    gjort lite

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         if (6 >= 3) {
             System.out.println("Hello, World!");
-
         }
-                }
+        Scanner tb = new Scanner(System.in);
+        String user = tb.nextLine();
+        if (user == "noname") {
+            System.out.println("Welcome");
+        }
+        
+    }
 }
